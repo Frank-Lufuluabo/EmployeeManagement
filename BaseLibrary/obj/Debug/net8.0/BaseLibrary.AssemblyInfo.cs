71dd681ac3e295dacae58ee06b80c8c2e79491a2
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-03-03 23:59:14 +0200

    Added features to Client

--- BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
+++ BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f734acebd585091d561a602bdd09140071a2a57f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c414470715c4baccd6198a02d0dbda2f6fdc3fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
