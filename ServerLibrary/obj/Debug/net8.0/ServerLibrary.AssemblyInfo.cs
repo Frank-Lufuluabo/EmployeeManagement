168cac05a636763cbab75eb088aa558b99612541
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-03-26 23:59:54 +0200

    Add new features

--- ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e54c51be067362fa573173abdc3a4cdfa183b5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3013e5c6beffc9af7bb73bbbe9885f8a6c41cb87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
